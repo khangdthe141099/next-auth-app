9a6d7d30248408fac98cb26e8395e358dc858ea0
ekoios-dev-khangdt <khang.dam@ekoios.vn>
2023-03-20 18:02:59 +0700

    commit

--- pages/login.js
+++ pages/login.js
@@ -1,40 +1,48 @@
 import Head from "next/head";
-import Link from "next/link"
+import Link from "next/link";
+import styles from "@/styles/Login.module.css";
 
 export default function Login() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Login</title>
       </Head>
 
-      <section>
-        <h1>Login form</h1>
+      <section className={styles.wrapper}>
+        <h1 className={styles.title}>Login form</h1>
 
-        <form>
-          <div>
-            <div>
-              <input type="email" name="email" placeholder="email" />
-            </div>
-            <div>
-              <input type="password" name="password" placeholder="password" />
-            </div>
+        <form className={styles.form}>
+          <input
+            className={styles.input}
+            type="email"
+            name="email"
+            placeholder="email"
+          />
+          <input
+            className={styles.input}
+            type="password"
+            name="password"
+            placeholder="password"
+          />
+
+          <div className={styles.btnGroup}>
+            <button className={styles.btn} type="submit">
+              Login
+            </button>
+            <button className={styles.btn} type="submit">
+              Sign in with Google
+            </button>
+            <button className={styles.btn} type="submit">
+              Sign in with Github
+            </button>
           </div>
 
-          <div>
-            <div>
-                <button type="submit">Logins</button>
-            </div>
-            <div>
-                <button type="submit">Sign in with Google</button>
-            </div>
-            <div>
-                <button type="submit">Sign in with Github</button>
-            </div>
-          </div>
-
-          <div>
-            don't have an account yet? <Link href="/register">Sign Up</Link>
+          <div className={styles.hint}>
+            don't have an account yet?{" "}
+            <Link className={styles.link} href="/register">
+              Sign Up
+            </Link>
           </div>
         </form>
       </section>
--- pages/register.js
+++ pages/register.js
@@ -1,30 +1,74 @@
 import Head from "next/head";
-import Link from "next/link"
+import Link from "next/link";
+import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
+import { useState } from "react";
+import styles from "@/styles/Login.module.css";
 
 export default function Register() {
+  const [show, setShow] = useState({
+    showPass: false,
+    showCPass: false,
+  });
+
+
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Register</title>
       </Head>
 
-      <section>
-        <h1>Register form</h1>
+      <section className={styles.wrapper}>
+        <h1 className={styles.title}>Register form</h1>
 
-        <form>
+        <form className={styles.form}>
+          <input className={styles.input} type="text" name="Username" placeholder="Username" />
+          <input className={styles.input} type="email" name="email" placeholder="Email" />
+          <div>
+          <input
+          className={styles.input}
+            type={`${show?.showPass ? "text" : "password"}`}
+            name="password"
+            placeholder="Password"
+          />
+          {show?.showPass ? (
+            <AiOutlineEye
+              onClick={(prev) => setShow({ ...prev, showPass: !show.showPass })}
+            />
+          ) : (
+            <AiOutlineEyeInvisible
+              onClick={(prev) => setShow({ ...prev, showPass: !show.showPass })}
+            />
+          )}
+          </div>
+          <div>
+          <input
+          className={styles.input}
+            type={`${show?.showCPass ? "text" : "password"}`}
+            name="cpassword"
+            placeholder="Confirm Password"
+          />
+          {show?.showCPass ? (
+            <AiOutlineEye
+              onClick={(prev) =>
+                setShow({ ...prev, showCPass: !show.showCPass })
+              }
+            />
+          ) : (
+            <AiOutlineEyeInvisible
+              onClick={(prev) =>
+                setShow({ ...prev, showCPass: !show.showCPass })
+              }
+            />
+          )}
+          </div>
 
           <div>
-            <div>
-              <input type="text" name="Username" placeholder="Username" />
-            </div>
-            <div>
-              <input type="email" name="email" placeholder="email" />
-            </div>
-            <div>
-              <input type={`${true ? "text" : "password"}`} name="password" placeholder="password" />
-            </div>
+            <button type="submit">Sign up</button>
           </div>
-          
+
+          <div>
+            You have already account? <Link href="/login">Login</Link>
+          </div>
         </form>
       </section>
     </div>
